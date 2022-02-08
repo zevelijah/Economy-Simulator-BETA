5a071fcd254a169c43c6a54bab0caa50b8c337ae
Zev Oster <73399535+zevelijah@users.noreply.github.com>
2022-02-08 16:47:28 -0500

    Add files via upload

--- Ecom.py
+++ Ecom.py
@@ -33,24 +33,24 @@ class Buyer: # The Buyer
         self.wfet3 = wfet3
         self.Buget = Buget # How much money they are CURRENTLY willing to buy at. 
 def taxCode(seller):
-    incomeTax = seller.inc * 0.3
+    incomeTax = seller.inc * 0.09
     seller.mon -= incomeTax
-    wealthTax = seller.mon * 0.25
+    wealthTax = seller.mon * 0.05
     seller.mon -= wealthTax
     salesTax = 0
     for sale in seller.trans:
-        tax = sale * 0.50
+        tax = sale * 0.1
         salesTax += tax
     seller.mon -= salesTax
-    # propertyValue = (seller.prod.fet1 * 0.5) + (seller.prod.fet2 * 0.8) + (seller.prod.fet3 * 1.2)
-    # if propertyValue <= 15:
-    #     seller.mon -= 400
-    # elif propertyValue <= 30:
-    #     seller.mon -= 600
-    # elif propertyValue <= 45:
-    #     seller.mon -= 800
-    # elif propertyValue <= 60:
-    #     seller.mon -= 1000
+    propertyValue = (seller.prod.fet1 * 0.5) + (seller.prod.fet2 * 0.8) + (seller.prod.fet3 * 1.2)
+    if propertyValue <= 15:
+        seller.mon -= 200
+    elif propertyValue <= 30:
+        seller.mon -= 300
+    elif propertyValue <= 45:
+        seller.mon -= 500
+    elif propertyValue <= 60:
+        seller.mon -= 750
     productionCosts = 0
     productionCosts += seller.prod.fet1 * 1.5
     productionCosts += seller.prod.fet2 * 4
@@ -60,6 +60,11 @@ def taxCode(seller):
     seller.trans = []
     return seller
 def Street(Buyers, Sellers): 
+    OringinalSellers = len(Sellers)
+    newBuyer = 1
+    newSeller = 1
+    numberBuyers = []
+    numberSellers = []
     failedSellers = [] # A list of sellers that failed to make a profit.
     failedBuyers = [] # A list of buyers that failed to find satifactory products.
     days = 0 # This is used to mesure how many days go on in the month.
@@ -67,23 +72,23 @@ def Street(Buyers, Sellers):
     months = 0 # How many months (31 days)have passed in the market
     budgets = [] # A list of average bugets each day.
     averageWealth = [] # A list of average wealth each day.
-    pricePoints = [] # A list of combined money of all stores on every day that has passed in the market.  
+    wealthPoints = [] # A list of combined money of all stores on every day that has passed in the market.  
     prices = [] # a list of the average price of every day that has passed in the market
     while True: # A loop defineing what happens on a given set of days
         days += 1 # A day starts, needs to be recoreded.
         day += 1 # A day starts, needs to be recoreded.
-        if days == 31: # if 31 days have passed since I last asked this question or since we started...
+        if days == 30: # if 31 days have passed since I last asked this question or since we started...
             for x in Sellers: # Every seller is...
                 taxCode(x) # ... taxed.
                 if x.mon < 0: # If they can't affored too...
-                    failedSellers.append((x.id, "failed to pay taxes", day)) # They are added to the failedSellers list with explanation.
+                    failedSellers.append(("failed to pay taxes", x.id, x.price, day)) # They are added to the failedSellers list with explanation.
                     Sellers.remove(x) # Their evicted.
             days = 0 # Resets days, justified because we add 1 to months
             months += 1
         if Sellers == []: # If there are no more sellers...
-            q = (months * 31 + days) - 1 # How many days have passed that there was market activity(This does not include today because the stores never opened)
+            q = day # How many days have passed that there was market activity(This does not include today because the stores never opened)
             # Creates a graph of Market Wealth and Average Market price vs days
-            plt.plot([x for x in range(q)], pricePoints, 'b', label = "Collective Coporate Wealth")
+            plt.plot([x for x in range(q)], wealthPoints, 'b', label = "Collective Coporate Wealth")
             plt.ylabel('Wealth of Market')
             plt.xlabel('Time(days)')
             plt.legend()
@@ -96,6 +101,11 @@ def Street(Buyers, Sellers):
             plt.show()
             plt.plot([x for x in range(q)], averageWealth, 'b', label = "Average Coparate Wealth")
             plt.ylabel('Average Wealth of Seller')
+            plt.xlabel('Time(days)')
+            plt.legend()
+            plt.show()
+            plt.plot([x for x in range(q)], numberBuyers, 'g', label = "Number of Buyers")
+            plt.ylabel('Number of Buyers')
             plt.xlabel('Time(days)')
             plt.legend()
             plt.show()
@@ -106,9 +116,9 @@ def Street(Buyers, Sellers):
             for man in Buyers:
                 names2.append((man.id, man.wfet1, man.wfet2, man.wfet3, man.Buget))
             return 'There is no place to buy from.', failedSellers, (len(names2), names2) # And as an added bonus, Tells us what happened
-        if months == 120: # After some amount of months...
-            q = (months * 31 + days) - 1 # How many days have passed that there was market activity(This does not include today because the stores never opened)
-            plt.plot([x for x in range(q)], pricePoints, 'b', label = "Collective Coporate Wealth")
+        if months == 600: # After some amount of months...
+            q = day - 1 # How many days have passed that there was market activity(This does not include today because the stores never opened)
+            plt.plot([x for x in range(q)], wealthPoints, 'b', label = "Collective Coporate Wealth")
             plt.ylabel('Wealth of Market')
             plt.xlabel('Time(days)')
             plt.legend()
@@ -124,13 +134,47 @@ def Street(Buyers, Sellers):
             plt.xlabel('Time(days)')
             plt.legend()
             plt.show()
+            plt.plot([x for x in range(q)], numberBuyers, 'g', label = "Number of Buyers")
+            plt.ylabel('Number of Buyers')
+            plt.xlabel('Time(days)')
+            plt.legend()
+            plt.show()
             names1 = []
             for man in Sellers:
-                names1.append((man.id, man.price))
+                names1.append((man.id, man.mon, man.price))
+            # I'll try this later
+            # sellers0 = []
+            # pricer = 2-1
+            # moneyer = 2-1
+            # numberer = 0
+            # searcher = []
+            # while numberer < len(names1):
+            #     pricer += 1
+            #     for y in names1:
+            #         numberer += 1
+            #         if y[2] == pricer:
+            #             searcher.append(y)
+            #         else:
+            #             numberer -= 1
+            #             looker = []
+            #             xx = 0
+            #             taker = 0
+            #             while xx < len(searcher):
+            #                 moneyer += 1
+            #                 for z in searcher:
+            #                     xx += 1
+            #                     if z[1] == moneyer:Aram
+            #                         looker.append(z)
+            #                     else:
+            #                         taker += 1
+            #                 xx -= taker
+            #                 sellers0.append(looker)
+            #                 looker = []
+            # I'll try this later
             names2 = []
             for man in Buyers:
                 names2.append((man.id, man.wfet1, man.wfet2, man.wfet3, man.Buget))
-            return (len(names1), failedSellers, names1), (len(names2) ,failedBuyers, names2) # Remaining sellers and buyers
+            return (len(names1), names1, failedSellers), (len(names2) ,names2, failedBuyers) # Remaining sellers and buyers
         for b in Buyers: # The buyers walk into the market
             yBug = False # When they check to see if a seller has a good price and they do, This says true.
             # Asks the same thing about the first two fetures fetures
@@ -150,7 +194,7 @@ def Street(Buyers, Sellers):
                                     s.inc += s.price # And adds the price to their monthly income tracker
                                     s.trans.append(s.price) # list the value of the transaction  
                                     s.quan -= 1 # There is one less product in inventory
-                                    s.vis = True # The Seller has made a transaction today
+                                    s.vis += 1 # The Seller has made a transaction today
                                     b.Buget = s.price # The buyer wants all products sold to him thereafter at this price. 
                                     break # The buy leaves the market for the day and the next one enters
                                 elif Sellers.index(s) == len(Sellers) - 1:# if Feture 3 is universal disatisfying...
@@ -197,7 +241,7 @@ def Street(Buyers, Sellers):
                         while True: # lower this number by two and and see if you can afford to fill up to it. do this until the seller finds this is impossible(and go out of buissness) or until reasonable price found.  
                             cap1 -= 2
                             if cap1 <= 0:
-                                failedSellers.append((s.id, s.price, s.prod.price9, s.cap, s.mon, "Cannot afford to restock"))
+                                failedSellers.append(("Cannot afford to restock", s.id, s.price, s.prod.price9, s.cap, s.mon, day))
                                 Sellers.remove(s)
                                 break
                             if s.prod.price9 * cap1 < s.mon: 
@@ -205,13 +249,10 @@ def Street(Buyers, Sellers):
                                 s.quan = cap1
                                 break
         for x in Sellers: # If the seller made a transaction today, set the Bollean that checks it to false and raise prices. if not, lower prices. 
-            if x.vis == True: 
-                x.vis = False
-                x.price += 5
-            else:
+            x.price += round(x.vis/30)
+            if x.vis == 0:
                 x.price -= 5
-                if x.price < x.min: # keep in mind that when lowered, prices can't be below mininum
-                    x.price = x.min
+            x.vis = 0
         MONe = 0 # How much money every seller has combined
         Qmon = [] # the prices of each seller
         bud = [] # the bugets of each buyer
@@ -222,28 +263,58 @@ def Street(Buyers, Sellers):
             wealth.append(x.mon) # Each sellers wealth is noted in this
         for x in Buyers: # The bugets of all the buyers are added up in this variable
             bud.append(x.Buget) # Each buyers price is noted in this list
+        wealthPoints.append(MONe) # The wealth of each seller is noted in this list
         averageWealth.append(stats.mean(wealth)) # The average wealth of the market is noted in this list
-        budgets.append(stats.mean(bud)) # The mean of the bugets is noted in this list
-        pricePoints.append(MONe) # pricePoints(The list recording market value on any given day) Takes the first vairable
-        prices.append(stats.mean(Qmon)) # prices records the average price of the stores that day
-        # ,Seller(25, 20, Product(8,2,2), 4000, 25, False, 25)
-        # Monopoly:Seller(10,y30,Product(10,5,3),40000,30,False,10)
+        budgets.append(stats.mean(bud)) # The mean of the bugets is noted in this lis wealthPoints.append(MONe) # wealthPoints(The list recording market value on any given day) Takes the first vairable
+        prices.append(stats.mean(Qmon)) # prices records the average price of the stores that          
+        if day % 5 == 0: 
+            chance = ri(0, 100) # A random number is generated
+            if chance >= 30:
+                Buyers.append(Buyer(str(newBuyer) + "nb", ri(3, 12), ri(3, 12), ri(3, 12), ri(50, 450), ri(150, 550))) # A new buyer is added to the market
+                newBuyer += 1
+            if chance >= 50:
+                Buyers.append(Buyer(str(newBuyer) + "nb", ri(3, 12), ri(3, 12), ri(3, 12), ri(50, 450), ri(150, 550))) # A new buyer is added to the market
+                newBuyer += 1
+            if chance >= 70:
+                Buyers.append(Buyer(str(newBuyer) + "nb", ri(3, 12), ri(3, 12), ri(3, 12), ri(50, 450), ri(150, 550))) # A new buyer is added to the market
+                newBuyer += 1
+            if chance >= 100:
+                Buyers.append(Buyer(str(newBuyer) + "nb", ri(3, 12), ri(3, 12), ri(3, 12), ri(50, 450), ri(150, 550))) # A new buyer is added to the market 
+                newBuyer += 1
+        if day % 5 == 0 and len(Sellers) < OringinalSellers: # If the number of sellers is less than the original number of sellers, a new seller is added to the market
+            Dif = OringinalSellers - len(Sellers)
+            for x in range(Dif):
+                ran = ri(0, 100)
+                if ran >= 40:
+                    a = ri(3, 12)
+                    b = ri(3, 12)
+                    c = ri(3, 12)
+                    Sellers.append(Seller(str(newSeller) + 'ns', ri(30, 550), ri(10, 20), Product(a, b, c), ri(1000, 15000), ri(10, 40), 0, ri(30, 250), 0, []))
+                    if Sellers[-1].min < Sellers[-1].prod.price9:
+                        Sellers[-1].min = Sellers[-1].prod.price9 + 10
+                    if Sellers[-1].price < Sellers[-1].min: 
+                        Sellers[-1].price = Sellers[-1].min + 10  # This is to make sure that the price is always higher than the minimum
+                    newSeller += 1
+        numberBuyers.append(len(Buyers)) # The number of buyers is noted in this list
+        numberSellers.append(len(Sellers)) # The number of sellers is noted in this list
 def peopleMaker(Buyers0, Sellers0):
     Buyers = []
     Sellers = []
     for buy in range(0, Buyers0):
         uiteration = str(buy) + "b"
         Buyers.append(Buyer(uiteration, ri(3, 12), ri(3, 12), ri(3, 12), ri(50, 450), ri(150, 550)))
+        if Buyers[buy].Buget > Buyers[buy].max:
+            Buyers[buy].Buget = Buyers[buy].max - 20
     for sell in range(0, Sellers0):
         a = ri(3, 12)
         b = ri(3, 12)
         c = ri(3, 12)
         uiteration = str(sell) + "s"
-        Sellers.append(Seller(uiteration, ri(30, 1000), ri(10, 20), Product(a, b, c), ri(1000, 15000), ri(10, 40), False, ri(30, 250), 0, []))
+        Sellers.append(Seller(uiteration, ri(30, 550), ri(10, 20), Product(a, b, c), ri(1000, 15000), ri(10, 40), 0, ri(30, 250), 0, []))
         if Sellers[sell].min < Sellers[sell].prod.price9:
             Sellers[sell].min = Sellers[sell].prod.price9 + 10
         if Sellers[sell].price < Sellers[sell].min: 
             Sellers[sell].price = Sellers[sell].min + 10  # This is to make sure that the price is always higher than the minimum   
     return Street(Buyers, Sellers)
-People = peopleMaker(300, 50)
+People = peopleMaker(1000, 100)
 print(People) 
